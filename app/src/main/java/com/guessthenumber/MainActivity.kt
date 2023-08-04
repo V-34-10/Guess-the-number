923aa4a71783ce18819ed1fbc7e9d514a8d2c934
Vlad <vladandryschenko98@gmail.com>
2023-08-04 18:18:41 +0300

    activity_main done, in process activity_game

--- app/src/main/java/com/guessthenumber/MainActivity.kt
+++ app/src/main/java/com/guessthenumber/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.guessthenumber
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AlphaAnimation
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        clickButton()
     }
+
+    private fun clickButton() {
+        val alphaAnimation = AlphaAnimation(1.0f, 0.0f)
+        alphaAnimation.duration = 2000
+        button_start.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+            button_start.startAnimation(alphaAnimation)
+            finish()
+        }
+        button_exit.setOnClickListener {
+            button_exit.startAnimation(alphaAnimation)
+            finish()
+        }
+    }
+
+    @Deprecated(
+        "Deprecated in Java",
+        ReplaceWith("super.onBackPressed()", "androidx.appcompat.app.AppCompatActivity")
+    )
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
 }
