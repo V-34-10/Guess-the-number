71d4db2ad9e0fb7be470c199bedd58d1d94c4ac7
Vlad <vladandryschenko98@gmail.com>
2023-08-06 18:24:34 +0300

    app done

--- app/src/main/java/com/guessthenumber/GameActivity.kt
+++ app/src/main/java/com/guessthenumber/GameActivity.kt
@@ -1,11 +1,55 @@
 package com.guessthenumber
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AlphaAnimation
+import kotlinx.android.synthetic.main.activity_game.*
+import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
+
+    private var secretNumber: Int = 0
+    private var attempts: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        clickButton()
+    }
+
+    private fun clickButton() {
+        secretNumber = Random.nextInt(1, 101)
+        val alphaAnimation = AlphaAnimation(1.0f, 0.0f)
+        alphaAnimation.duration = 2000
+        checkButton.setOnClickListener {
+            val input = number_edit.text.toString().toIntOrNull()
+            checkButton.startAnimation(alphaAnimation)
+            if (input != null) {
+                attempts++
+                if (input == secretNumber) {
+                    val message =
+                        getString(R.string.congratulations_message, secretNumber, attempts)
+                    result_text.text = message
+                    resultButton.visibility = View.VISIBLE
+                    checkButton.isEnabled = false
+                } else if (input < secretNumber) {
+                    result_text.text = getString(R.string.greater_number_message)
+                } else {
+                    result_text.text = getString(R.string.smaller_number_message)
+                }
+            } else {
+                result_text.text = getString(R.string.enter_number_message)
+            }
+        }
+
+        resultButton.setOnClickListener {
+            resultButton.startAnimation(alphaAnimation)
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("attempts", attempts)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/guessthenumber/ResultActivity.kt
+++ app/src/main/java/com/guessthenumber/ResultActivity.kt
@@ -1,11 +1,29 @@
 package com.guessthenumber
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AlphaAnimation
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        getResult()
+    }
+
+    private fun getResult() {
+        val alphaAnimation = AlphaAnimation(1.0f, 0.0f)
+        alphaAnimation.duration = 2000
+        val attempts = intent.getIntExtra("attempts", 0)
+        val message = getString(R.string.count_guess_number, attempts)
+        resultTextView.text = message
+        return_to_menu_button.setOnClickListener {
+            return_to_menu_button.startAnimation(alphaAnimation)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
